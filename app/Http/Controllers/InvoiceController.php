abea5758340dcceebea3bb628a7fb8f796a8446d
rayhanbabu <rayhanbabu458@gmail.com>
2023-11-08 19:28:42 +0600

    curl update

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -53,61 +53,74 @@ class InvoiceController extends Controller
       $signature_key = "dbb74894e82415a2f7ff0ec3a97e4183";
       $url = "https://​sandbox​.aamarpay.com/jsonpost.php"; // for Live Transection use "https://secure.aamarpay.com/jsonpost.php"
 
+
       $curl = curl_init();
 
-      curl_setopt_array($curl, array(
-        CURLOPT_URL => $url,
+      curl_setopt($curl, CURLOPT_URL, $url);
+
+      // Set other cURL options as an array
+      $options = array(
         CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_ENCODING => '',
+        CURLOPT_ENCODING => "",
         CURLOPT_MAXREDIRS => 10,
         CURLOPT_TIMEOUT => 0,
         CURLOPT_FOLLOWLOCATION => true,
-        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
         CURLOPT_CUSTOMREQUEST => 'POST',
         CURLOPT_POSTFIELDS => '{
-        "store_id": "' . $store_id . '",
-        "tran_id": "' . $tran_id . '",
-        "success_url": "' . route('amarpay_success') . '",
-        "fail_url": "' . route('amarpay_fail') . '",
-        "cancel_url": "' . route('amarpay_cancel') . '",
-        "amount": "' . $amount . '",
-        "currency": "' . $currency . '",
-        "signature_key": "' . $signature_key . '",
-        "desc": "' . $invoice->category_id . '",
-        "cus_name": "' . $member->name . '",
-        "cus_email": "' . $member->email . '",
-        "cus_add1": "' . $member->id . '",
-        "cus_add2": "Mohakhali DOHS",
-        "cus_city": "' . $member->city . '",
-        "cus_state": "' . $member->city . '",
-        "cus_postcode": "1206",
-        "cus_country": "' . $member->country . '",
-        "cus_phone": "' . $member->phone . '",
-        "opt_a":"' . $invoice->id . '" ,
-        "opt_b":"' . $admin->other_link . '" ,
-        "opt_c":"' . $admin->admin_name . '" ,
-        "type": "json"
-    }',
+          "store_id": "' . $store_id . '",
+          "tran_id": "' . $tran_id . '",
+          "success_url": "' . route('amarpay_success') . '",
+          "fail_url": "' . route('amarpay_fail') . '",
+          "cancel_url": "' . route('amarpay_cancel') . '",
+          "amount": "' . $amount . '",
+          "currency": "' . $currency . '",
+          "signature_key": "' . $signature_key . '",
+          "desc": "' . $invoice->category_id . '",
+          "cus_name": "' . $member->name . '",
+          "cus_email": "' . $member->email . '",
+          "cus_add1": "' . $member->id . '",
+          "cus_add2": "Mohakhali DOHS",
+          "cus_city": "' . $member->city . '",
+          "cus_state": "' . $member->city . '",
+          "cus_postcode": "1206",
+          "cus_country": "' . $member->country . '",
+          "cus_phone": "' . $member->phone . '",
+          "opt_a":"' . $invoice->id . '" ,
+          "opt_b":"' . $admin->other_link . '" ,
+          "opt_c":"' . $admin->admin_name . '" ,
+          "type": "json"
+       }',
+        // Replace $postData with your data array
         CURLOPT_HTTPHEADER => array(
           'Content-Type: application/json'
         ),
-      ));
+      );
 
+      // Set the cURL options using curl_setopt_array
+      curl_setopt_array($curl, $options);
+
+      // Execute the cURL request
       $response = curl_exec($curl);
 
-      curl_close($curl);
-      //dd($response);
-
-      $responseObj = json_decode($response);
-
-      if (isset($responseObj->payment_url) && !empty($responseObj->payment_url)) {
-
-        $paymentUrl = $responseObj->payment_url;
-        // dd($paymentUrl);
-        return redirect()->away($paymentUrl);
+      // Handle the response and any errors
+      if ($response === false) {
+        // Handle cURL error
+        echo 'cURL Error: ' . curl_error($curl);
       } else {
-        echo $response;
+
+        $responseObj = json_decode($response);
+        if (isset($responseObj->payment_url) && !empty($responseObj->payment_url)) {
+
+          $paymentUrl = $responseObj->payment_url;
+          // dd($paymentUrl);
+          return redirect($paymentUrl);
+        } else {
+          echo $response;
+        }
       }
+
+      // Close the cURL session
+      curl_close($curl);
     } catch (Exception $e) {
       return "Something Error. please try again";
     }
@@ -123,51 +136,59 @@ class InvoiceController extends Controller
 
       $url = "http://sandbox.aamarpay.com/api/v1/trxcheck/request.php?request_id=$request_id&store_id=aamarpaytest&signature_key=dbb74894e82415a2f7ff0ec3a97e4183&type=json";
 
-      //For Live Transection Use "http://secure.aamarpay.com/api/v1/trxcheck/request.php"
 
       $curl = curl_init();
+      curl_setopt($curl, CURLOPT_URL, $url);
 
-      curl_setopt_array($curl, array(
-        CURLOPT_URL => $url,
+      // Set other cURL options
+      $options = array(
         CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_ENCODING => '',
-        CURLOPT_MAXREDIRS => 10,
-        CURLOPT_TIMEOUT => 0,
         CURLOPT_FOLLOWLOCATION => true,
-        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        CURLOPT_CUSTOMREQUEST => 'GET',
-      ));
+        CURLOPT_HTTPHEADER => array(
+          'Content-Type: application/json', // Add any headers required
+        ),
+      );
 
+      // Set the cURL options using curl_setopt_array
+      curl_setopt_array($curl, $options);
+
+      // Execute the cURL request
       $response = curl_exec($curl);
 
+      // Handle the response and any errors
+      if ($response === false) {
+        // Handle cURL error
+        echo 'cURL Error: ' . curl_error($curl);
+      } else {
+        
+        $success = json_decode($response, true);
+        //echo $success['status_code'];
+        $payment_date = date('Y-m-d', strtotime($success['date_processed']));
+        $payment_day = date('d', strtotime($success['date_processed']));
+        $payment_month = date('n', strtotime($success['date_processed']));
+        $payment_year = date('Y', strtotime($success['date_processed']));
+  
+        $model = Invoice::find($success['opt_a']);
+        $model->payment_status = 1;
+        $model->payment_type = 'Online';
+        $model->payment_time = $success['date_processed'];
+        $model->payment_method = $success['payment_type'];
+        $model->payment_date = $payment_date;
+        $model->payment_year = $payment_year;
+        $model->payment_month = $payment_month;
+        $model->payment_day = $payment_day;
+        $model->update();
+        return view('web.payment_success', ["web_link" => $success['opt_b']]);
+
+      }
+
+      // Close the cURL session
       curl_close($curl);
-      // echo  $response;
 
       //database working part 
-      $success = json_decode($response, true);
-      //echo $success['status_code'];
-      $payment_date = date('Y-m-d', strtotime($success['date_processed']));
-      $payment_day = date('d', strtotime($success['date_processed']));
-      $payment_month = date('n', strtotime($success['date_processed']));
-      $payment_year = date('Y', strtotime($success['date_processed']));
-
-     
-
-      $model = Invoice::find($success['opt_a']);
-      $model->payment_status = 1;
-      $model->payment_type = 'Online';
-      $model->payment_time = $success['date_processed'];
-      $model->payment_method = $success['payment_type'];
-      $model->payment_date = $payment_date;
-      $model->payment_year = $payment_year;
-      $model->payment_month = $payment_month;
-      $model->payment_day = $payment_day;
-      $model->update();
-
-  
-      return view('web.payment_success', ["web_link" => $success['opt_b']]);
     } catch (Exception $e) {
-     return "Something Error. please try again"; }
+      return "Something Error. please try again";
+    }
   }
 
 
@@ -197,9 +218,9 @@ class InvoiceController extends Controller
       $signature_key = "dbb74894e82415a2f7ff0ec3a97e4183";
 
       $url = "http://sandbox.aamarpay.com/api/v1/trxcheck/request.php?request_id=$tran_id&store_id=$store_id&signature_key=$signature_key&type=json";
- 
-       //return $url;
-     // die();
+
+      //return $url;
+      // die();
       $curl = curl_init();
 
       curl_setopt_array($curl, array(
@@ -216,13 +237,13 @@ class InvoiceController extends Controller
       $response = curl_exec($curl);
 
       curl_close($curl);
-   
+
       $data = json_decode($response, true);
-    if ($data === null) {
+      if ($data === null) {
         echo "JSON decoding failed.";
-    } else {
-      return view('admin.invoice_search', ["data" =>$data]);
-     }
+      } else {
+        return view('admin.invoice_search', ["data" => $data]);
+      }
     } catch (Exception $e) {
       return "Something Error. please try again";
     }
